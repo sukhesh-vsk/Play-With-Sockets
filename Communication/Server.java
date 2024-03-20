072bf0b01fc4e8b7d2debc5edf73c22410906860
sukhesh-vsk <115344170+sukhesh-vsk@users.noreply.github.com>
2024-03-20 09:52:46 +0530

    Updated file

--- Communication/Server.java
+++ Communication/Server.java
@@ -1,1 +1,31 @@
+import java.net.*;
+import java.util.*;
 
+public class Server {
+    private static String _ipaddr = "localhost";
+    private static int _port = 1234;
+
+    public static void main(String args []) {
+        try (ServerSocket server = new ServerSocket(_port)){
+            Socket s = server.accept();
+            System.out.println("Client Connected");
+
+            while(true) {
+                Scanner sc = new Scanner(s.getInputStream());
+                String msg = sc.next();
+
+                if(msg.equals("bye")) {
+                    System.out.println("------------- Connection closed -------------");
+                    s.close();
+                    server.close();
+                    return;
+                }
+                System.out.println("Client Message : "+ msg);
+
+            }
+        }
+        catch(Exception e) {
+            System.out.println("Connection error");
+        }
+    }
+}
